4e0a3cacfad310a3d5edfcb9a53ef62f53b895e3
digvijaykarande <karandedigvijay2@gmail.com>
2025-07-17 14:45:08 +0530

    download route added

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
 const app = express();
+const fs = require('fs');
+const router = express.Router();
 const PORT = process.env.PORT || 8000;
 
 dotenv.config();
-
 // Connect MongoDB
 connectDB();
+
+//rate limiter
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 50, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+});
+
+app.use(limiter);
 
 // Middlewares
 app.use(cors()); //allowed access to all ( only for testing)
@@ -23,5 +33,15 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/excel", require("./routes/excelRoutes"));
 app.use("/api/excel/files", require("./routes/excelRoutes"));
 
+//download files 
+router.get('/api/excel/download/:id', async (req, res) => {
+  const fileId = req.params.id;
+  const fileRecord = await FileModel.findById(fileId);
+  if (!fileRecord) {
+    return res.status(404).send('File not found');
+  }
+  const filePath = path.join(__dirname, '..', 'uploads', fileRecord.filename);
+  res.download(filePath, fileRecord.filename);
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
